e3bfc95b6d2a10a423deb66bfb269cbe09dadba4
LotusTerHaar <lotus.ter.haar@gmail.com>
2019-05-11 20:32:49 +0200

    @EnableJpaRepositories path veranderd

--- src/main/java/han/oose/dea/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/han/oose/dea/springbootdemo/SpringBootDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("han.oose.dea.persistence.repo")
+@EnableJpaRepositories("han.oose.dea.springbootdemo.persistence.repo")
 @EntityScan("org.baeldung.persistence.model")
 @SpringBootApplication
 public class SpringBootDemoApplication {
