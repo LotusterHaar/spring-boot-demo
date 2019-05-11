ec24ce25d76ded76a786b703b318b01d62fadff6
LotusTerHaar <lotus.ter.haar@gmail.com>
2019-05-11 21:30:08 +0200

    update import statements

--- src/main/java/han/oose/dea/springbootdemo/Application.java
+++ src/main/java/han/oose/dea/springbootdemo/Application.java
@@ -3,15 +3,18 @@ package han.oose.dea.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
+@SpringBootApplication(scanBasePackages = "han.oose.dea.springbootdemo")
 @EnableJpaRepositories("han.oose.dea.springbootdemo.persistence.repo")
 @EntityScan("han.oose.dea.springbootdemo.persistence.model")
-@SpringBootApplication
-public class SpringBootDemoApplication {
+
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/han/oose/dea/springbootdemo/persistence/repo/BookRepository.java
+++ src/main/java/han/oose/dea/springbootdemo/persistence/repo/BookRepository.java
@@ -4,6 +4,7 @@ import han.oose.dea.springbootdemo.persistence.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
